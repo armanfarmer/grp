e1c64e7bc6cf1580f752af6a7a786019115b1942
Crypto Dypto <team@deltaone.xyz>
2023-05-14 13:25:56 +0200

    updates

--- src/styles.ts
+++ src/styles.ts
@@ -1,21 +1,23 @@
-.App {
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
   text-align: center;
   font-family: Arial, sans-serif;
   background-color: #f2f2f2;
-}
+`;
 
-.App-header {
+export const Header = styled.header`
   background-color: #282c34;
   padding: 20px;
   color: white;
-}
+`;
 
-.section {
+export const Section = styled.section`
   padding: 20px;
-}
+`;
 
-footer {
+export const Footer = styled.footer`
   background-color: #282c34;
   padding: 20px;
   color: white;
-}
+`;
